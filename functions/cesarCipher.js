00d84c837f14263f847a83b93ae1dd17144adb0d
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2023-12-21 19:33:32 +0100

    added option for punctuation

--- functions/cesarCipher.js
+++ functions/cesarCipher.js
@@ -1,13 +1,13 @@
 function cesarCipher(word) {
-  let cipher = word.split("");
-  let cipherArray = [];
-
-  let alphabet = [..."ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+  const cipher = word.split("");
+  const cipherArray = [];
+  const alphabet = [..."ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+  const punctuationRegEx = /[^\w\s]+/g;
 
   const findLetterIndex = cipher.map((letter) => alphabet.indexOf(letter));
 
   for (let i = 0; i < cipher.length; i++) {
-    if (cipher[i] !== " ") {
+    if (cipher[i] !== " " && !punctuationRegEx.test(cipher[i])) {
       if (findLetterIndex[i] - 3 < 0) cipherArray.push(alphabet[findLetterIndex[i] - 3 + alphabet.length]);
       else cipherArray.push(alphabet[findLetterIndex[i] - 3]);
     } else cipherArray.push(cipher[i]);
